ee90dc6dd3d5aff72ffb4fa09e97696532b4ab4a
Andy Junhyuk Park <34406299+pos03142ap@users.noreply.github.com>
2021-09-16 02:53:52 -0700

    Added search feature Axios via Nasa Images API

---
+++ src/components/Search.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="search">
+    <h2>SPACESTAGRAM</h2>
+    <form v-on:submit.prevent="getResult(query)">
+      <input type="text" placeholder="Search" v-model="query" />
+    </form>
+    <div v-if="results">
+      <div v-for="result in results" :key="result">
+        <img v-bind:src="result.links[0].href" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  name: 'search',
+  data () {
+    return {
+      msg:'Search',
+      query:'',
+      results: ''
+    }
+  },
+  methods: {
+    getResult(query) {
+      axios.get('https://images-api.nasa.gov/search?q=' + query + '&media_type=image')
+      .then(response => {
+        console.log(response.data.collection.items);
+        this.results = response.data.collection.items;
+      });
+    }
+  } 
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
