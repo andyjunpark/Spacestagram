e7f1fc8f3e07a482894614b7b2729bb0671fe6ff
Andy Junhyuk Park <34406299+pos03142ap@users.noreply.github.com>
2021-09-16 05:52:21 -0700

    Added cards to search results

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,16 +1,34 @@
 <template>
   <div class="search">
     <h2>SPACESTAGRAM</h2>
-    <fas icon="fa-coffee" />
-    <form v-on:submit.prevent="getResult(query)">
-      <i class="fas fa-search"></i>
-      <input type="text" placeholder="Search" v-model="query" />
-    </form>
+  
+  <!-- Search Button -->
+  <div class="boxContainer">
+    <table class="elementsContainer">
+      <tr>
+        <td>
+            <form v-on:submit.prevent="getResult(query)">
+              <input type="text" placeholder="Search" class="search" v-model="query" />
+              <a href="#">
+                <i class="fas fa-search"></i>
+              </a>
+            </form>
+        </td>
+      </tr>
+    </table>
+  </div>
+
+  <!-- Get Results-->
     <div class="results" v-if="results">
       <div v-for="result in results" :key="result">
-        <img v-bind:src="result.links[0].href" />
+        <div class="card">
+          <img v-bind:src="result.links[0].href" />
+          <h4>{{result.data[0].title}}</h4>
+          <p>{{result.data[0].date_created}}</p>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -38,16 +56,23 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-.results img {
-  max-width:400px;
-  margin:10px;
-  border-radius:15px;
+/* Texts*/
+h2 {
+  color:black;
 }
 
 h3 {
   margin: 40px 0 0;
 }
+
+h4 {
+  margin: 20px 0 0;
+  color:black;
+  opacity:90%;
+}
+
 ul {
   list-style-type: none;
   padding: 0;
@@ -56,7 +81,68 @@ li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+  color:#000000;
+  opacity:70%;
 }
+
+p {
+  padding:5%;
+  color:black;
+  opacity:90%;
+}
+/* Result Cards*/
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.4);
+  transition: 0.5s;
+  width:400px;
+  margin-top:5%;
+  margin-left:auto;
+  margin-right:auto;
+  border-radius:10px;
+  background-color: white;
+}
+
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.4);
+}
+
+.results img {
+  max-width:380px;
+  margin:0px;
+  padding:10px;
+}
+
+.boxContainer {
+  margin:auto;
+  margin-top:10%;
+  position:relative;
+  width:300px;
+  height:42px;
+  border: 2px solid #000000;
+  padding:0px 10px;
+  border-radius: 50px;
+  opacity:80%;
+}
+
+.elementsContainer {
+  width: 100%;
+  height: 100%;
+  vertical-align: middle;
+}
+
+.search {
+  border: none;
+  height: 100%;
+  border-radius: 50px;
+  font-size:18px;
+  color: #424242;
+  font-weight: 500;
+}
+
+.search:focus {
+  outline:none;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-import { createApp } from 'vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-import App from './App.vue';
-
-library.add(fas);
+import { createApp } from 'vue'
+import App from './App.vue'
 
 createApp(App).mount('#app')
-    .component('fa', FontAwesomeIcon)
-    .mount('#app')
